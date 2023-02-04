d552acd52be693f51468478eb7da0f78f2f44eab
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2023-02-04 21:55:23 +0700

    fix(wall): get after wall fixed

--- posts/wall_urls.py
+++ posts/wall_urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     re_path(r'/?$', views.PostsGetAllWallView.as_view()),
     re_path(r'/latest/?$', views.PostsGetLatestByAuthorView.as_view()),
-    re_path(r'/(?P<post_id>\d+)/after/?$', views.PostsGetAllWallView.as_view()),
+    re_path(r'/(?P<post_id>\d+)/after/?$', views.PostsGetAfterWallView.as_view()),
     re_path(r'/(?P<post_id>\d+)/before/?$', views.PostsGetBeforeWallView.as_view()),
     re_path(r'/(?P<post_id>\d+)/newer/?$', views.PostsGetNewerWallView.as_view()),
 ]
