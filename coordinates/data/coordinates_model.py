89b4812f44063aa7c8383dafd3e601fcb74db3fb
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2023-08-12 16:37:10 +0700

    fix: Coordinates fix 2 digits before the decimal point

--- coordinates/data/coordinates_model.py
+++ coordinates/data/coordinates_model.py
@@ -4,8 +4,8 @@ from coordinates.domain.coordinates_dto import CoordinatesDto
 
 
 class CoordinatesModel(models.Model):
-    lat = models.DecimalField(decimal_places=20, max_digits=22)
-    long = models.DecimalField(decimal_places=20, max_digits=22)
+    lat = models.DecimalField(decimal_places=20, max_digits=23)
+    long = models.DecimalField(decimal_places=20, max_digits=23)
 
     def to_dto(self) -> CoordinatesDto:
         return CoordinatesDto(lat=self.lat, long=self.long)
--- coordinates/serilizers.py
+++ coordinates/serilizers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 
 
 class CoordinatesSerializer(serializers.Serializer):
-    lat = serializers.DecimalField(decimal_places=20, max_digits=22)
-    long = serializers.DecimalField(decimal_places=20, max_digits=22)
+    lat = serializers.DecimalField(decimal_places=20, max_digits=23)
+    long = serializers.DecimalField(decimal_places=20, max_digits=23)
